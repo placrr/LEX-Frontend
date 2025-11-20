84585f99691898f953566fe58edf4ac284643a26
Sujay kumar <sujayraw13@gmail.com>
2025-11-20 20:05:36 +0530

    unstage .next folder

--- .next/dev/static/chunks/894cb_next_error_3639f1f2.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/OneDrive/Desktop/Projects/LEX/node_modules/next/error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/OneDrive/Desktop/Projects/LEX/node_modules/next/dist/pages/_error.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=894cb_next_error_3639f1f2.js.map
